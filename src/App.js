735db1dc9abf18535ccd684d9832b1811edd25d4
Edward Odhiambo <Edward@Macs-MacBook-Air.local>
2022-05-10 14:05:50 +0300

    Add Header

--- src/App.js
+++ src/App.js
@@ -2,7 +2,29 @@ import './App.css';
 
 function App() {
   return (
-    <h1 style={{textAlign: 'center'}}>Hello Ezekiel and Edward</h1>
+    <div className='ui container'>
+      <div className="ui segment">
+        <h1>Hello Ezekiel and Edward</h1>
+        
+        <form class="ui form">
+          <div class="field">
+            <label>First Name</label>
+            <input placeholder="First Name" />
+          </div>
+          <div class="field">
+            <label>Last Name</label>
+            <input placeholder="Last Name" />
+          </div>
+          <div class="field">
+            <div class="ui checkbox">
+              <input type="checkbox" class="hidden" readonly="" tabindex="0" />
+              <label>I agree to the Terms and Conditions</label>
+            </div>
+          </div>
+          <button class="ui button" type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
   );
 }
 
